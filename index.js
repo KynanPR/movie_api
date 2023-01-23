1e34ce0ffa2b221713fda7b89675e97628314097
KynanPR <kynanpr@gmail.com>
2023-01-23 17:41:54 +0000

    Added endpoints
    Without handler functions

--- index.js
+++ index.js
@@ -60,8 +60,48 @@ app.get('/documentation', (req, res) => {
     res.sendFile('public/documentation.html', {root:__dirname});
 });
 
-app.get('/movies', (req, res) => {
-    res.json(movies);
+app.get('/data/movies', (req, res) => {
+    res.send('All movies.json');
+});
+
+app.get('/data/movies/:title', (req, res) => {
+    res.send(req.params.title + '.json');
+});
+
+app.get('/data/genres/:genre', (req, res) => {
+    res.send(req.params.genre + '.json');
+});
+
+app.get('/data/directors/:director', (req, res) => {
+    res.send(req.params.director + '.json');
+});
+
+app.post('/users/register', (req, res) => {
+    res.send('Registering!');
+});
+
+app.post('/users/login/:username', (req, res) => {
+    res.send(req.params.username + '.json');
+});
+
+app.get('/users/:username', (req, res) => {
+    res.send(req.params.username + '.json');
+});
+
+app.put('/users/:username', (req, res) => {
+    res.send(req.params.username + '.json updated');
+});
+
+app.post('/users/:username/favorites/:movieId', (req, res) => {
+    res.send(req.params.username + '_fav.json - ' + req.params.movieId + ' added');
+});
+
+app.delete('/users/:username/favorites/:movieId', (req, res) => {
+    res.send(req.params.username + '_fav.json - ' + req.params.movieId + ' removed');
+});
+
+app.delete('/users/:username', (req, res) => {
+    res.send(req.params.username + ' deleted');
 });
 
 app.use(express.static('public'));
