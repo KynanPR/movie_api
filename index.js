ae08a5a0b48e7cc7b2d0aeae9d5e43c7eac2a472
KynanPR <kynanpr@gmail.com>
2023-05-22 18:06:12 +0100

    Removed Hardcoded Test Data

--- index.js
+++ index.js
@@ -7,49 +7,6 @@ const app = express();
 
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.log'), {flags: 'a'});
 
-let movies = [
-    {
-        name: 'Good Will Hunting',
-        releaseYear: '1998',
-    },
-    {
-        name: 'Pitch Perfect',
-        releaseYear: '2012',
-    },
-    {
-        name: 'I don\'t watch that many films',
-        releaseYear: '1999',
-    },
-    {
-        name: 'I don\'t watch that many films: Part 2 Electric Boogaloo',
-        releaseYear: '1999+1',
-    },
-    {
-        name: 'The Room',
-        releaseYear: '2009',
-    },
-    {
-        name: 'Emoji Movie',
-        releaseYear: '2017',
-    },
-    {
-        name: 'Cats',
-        releaseYear: '2019',
-    },
-    {
-        name: 'The Arrival of a Train',
-        releaseYear: '1896',
-    },
-    {
-        name: 'Pitch Perfect 2',
-        releaseYear: '2015',
-    },
-    {
-        name: 'This was the hardest part of this excercise',
-        releaseYear: '2022',
-    },
-];
-
 app.use(morgan('combined', {stream: accessLogStream}));
 
 app.get('/', (req, res) => {
